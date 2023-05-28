5a896e95d3178931017fc5407fab05f33f5f99ba
zakie nobakht <zakie.nobakht@gmail.com>
2023-05-29 01:21:27 +0430

    git file address edited

---
+++ src/GitLink.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function GitLink() {
+  return (
+    <div>
+      <a href="https://github.com/znobakht/react" target="_blank">
+        {" "}
+        GitHub
+      </a>
+    </div>
+  );
+}
