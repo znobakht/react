38c1eac5e31548cc9ad858dcfd8c14949fc8ac2c
zakie nobakht <zakie.nobakht@gmail.com>
2023-05-29 01:12:04 +0430

    git link added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Search from './Search';
+import reportWebVitals from "./reportWebVitals";
+import Search from "./Search";
+import GitLink from "./gitLink";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <App /> */}
     <Search />
+    <GitLink />
   </React.StrictMode>
 );
 
